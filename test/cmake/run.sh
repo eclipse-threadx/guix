f2658d8fb3c9ed798a3eac74dc3cdf2b36730937
tinzhu <tinzhu@microsoft.com>
2023-10-17 15:07:28 +0800

    Update the path of "cmake_bootstrap.sh" file.

--- test/cmake/run.sh
+++ test/cmake/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd $(dirname $0)
-[ -f .run.sh ] || ln -sf ../../externals/threadx/.build/cmake_bootstrap.sh .run.sh
+[ -f .run.sh ] || ln -sf ../../externals/threadx/scripts/cmake_bootstrap.sh .run.sh
 ./.run.sh $*
