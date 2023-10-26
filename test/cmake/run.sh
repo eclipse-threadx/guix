c4f82d9f6115bd729511f1b7b0bb9eb06dbaab00
tinzhu <tinzhu@microsoft.com>
2023-10-26 18:34:34 +0800

    Generate Junit test report.

--- test/cmake/run.sh
+++ test/cmake/run.sh
@@ -2,6 +2,13 @@
 
 cd $(dirname $0)
 [ -f .run.sh ] || ln -sf ../../externals/threadx/scripts/cmake_bootstrap.sh .run.sh
+
+# find line that contains "ctest $parallel" in .run.sh
+line=$(grep -n "ctest \$parallel" .run.sh | cut -d: -f1)
+
+# Inser "--output-junit $1.xml" after the line
+sed -i "${line}a \\\t--output-junit $1.xml" .run.sh
+
 ./.run.sh $*
 
 #!/bin/bash
