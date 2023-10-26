733f0f3b9003c364f262634de98ab40db62bc268
tinzhu <tinzhu@microsoft.com>
2023-10-26 18:23:14 +0800

    Update run.sh

--- test/cmake/run.sh
+++ test/cmake/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd $(dirname $0)
-#[ -f .run.sh ] || ln -sf ../../externals/threadx/scripts/cmake_bootstrap.sh .run.sh
+[ -f .run.sh ] || ln -sf ../../externals/threadx/scripts/cmake_bootstrap.sh .run.sh
 ./.run.sh $*
 
 #!/bin/bash
