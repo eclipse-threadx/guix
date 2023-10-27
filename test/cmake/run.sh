1016058435de22a6d6cecf6eb8291e402849be0a
tinzhu <tinzhu@microsoft.com>
2023-10-27 10:00:01 +0800

    Correct logic.

--- test/cmake/run.sh
+++ test/cmake/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 cd $(dirname $0)
-[ -f .run.sh ] || ln -sf ../../externals/threadx/scripts/cmake_bootstrap.sh .run.sh
 
 # find line that contains "ctest $parallel" in .run.sh
-line=$(grep -n "ctest \$parallel" .run.sh | cut -d: -f1)
+line=$(grep -n "ctest \$parallel" ../../externals/threadx/scripts/cmake_bootstrap.sh | cut -d: -f1)
 
-# Inser "--output-junit $1.xml" after the line
-sed -i "${line}a \\\t--output-junit $1.xml" .run.sh
+# Add "--output-junit $1.xml" to the end of the line
+sed -i "${line}s/$/ --output-junit $1.xml/" ../../externals/threadx/scripts/cmake_bootstrap.sh
 
+[ -f .run.sh ] || ln -sf ../../externals/threadx/scripts/cmake_bootstrap.sh .run.sh
 ./.run.sh $*
 
 #!/bin/bash
