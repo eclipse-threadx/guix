f74bc92c817c52016a64a79631dd552d9c3c1468
tinzhu <tinzhu@microsoft.com>
2023-11-10 15:49:10 +0800

    Update instal.sh.

--- scripts/install.sh
+++ scripts/install.sh
@@ -19,10 +19,9 @@ sudo apt-get install -y \
     gawk \
     software-properties-common
 
-sudo wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | sudo apt-key add -
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | sudo apt-key add -
 CODENAME=$(lsb_release -c | cut -f2 -d':' | sed 's/\t//')
 apt-add-repository "deb https://apt.kitware.com/ubuntu/ $CODENAME main"
-sudo apt-get -y install cmake
 
 python3 -m pip install --upgrade pip
 pip3 install gcovr==4.1
