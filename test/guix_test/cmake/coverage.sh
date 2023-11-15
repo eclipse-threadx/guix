e51fac41bcd913746746830885d8234ce0752b61
tinzhu <tinzhu@microsoft.com>
2023-11-15 08:35:28 +0000

    Fix path.

--- test/guix_test/cmake/coverage.sh
+++ test/guix_test/cmake/coverage.sh
@@ -4,5 +4,5 @@ set -e
 
 cd $(dirname $0)
 mkdir -p coverage_report/$1
-gcovr --object-directory=build/$1/guix/CMakeFiles/guix.dir/common/src -r ../../common/src --xml-pretty --output coverage_report/$1.xml
-gcovr --object-directory=build/$1/guix/CMakeFiles/guix.dir/common/src -r ../../common/src --html --html-details --output coverage_report/$1/index.html
+gcovr --object-directory=build/$1/guix/CMakeFiles/guix.dir/common/src -r ../../../common/src --xml-pretty --output coverage_report/$1.xml
+gcovr --object-directory=build/$1/guix/CMakeFiles/guix.dir/common/src -r ../../../common/src --html --html-details --output coverage_report/$1/index.html
