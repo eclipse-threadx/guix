49182602d1ee74d433606de50dd61ae1217418f6
Ting Zhu <tinzhu@microsoft.com>
2023-11-16 13:37:36 +0800

    Update run.sh
    Update script.

--- test/guix_test/cmake/run.sh
+++ test/guix_test/cmake/run.sh
@@ -6,12 +6,6 @@ cd $(dirname $0)
 [ -d threadx ] ||
 git clone https://github.com/azure-rtos/threadx.git
 
-# find line that contains "ctest $parallel" in .run.sh
-line=$(grep -n "ctest \$parallel" threadx/scripts/cmake_bootstrap.sh | cut -d: -f1)
-
-# Add "--output-junit $1.xml" to the end of the line
-sed -i "${line}s/$/ --output-junit $1.xml/" threadx/scripts/cmake_bootstrap.sh
-
 [ -f .run.sh ] || ln -sf threadx/scripts/cmake_bootstrap.sh .run.sh
 ./.run.sh $*
 
