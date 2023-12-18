8c9f3664f0a31c8ca62f8c9750bed9bfd0ec7eee
tinzhu <tinzhu@microsoft.com>
2023-12-18 17:00:03 +0800

    Update function header.

--- common/src/gx_vertical_list_scroll_info_get.c
+++ common/src/gx_vertical_list_scroll_info_get.c
@@ -35,7 +35,7 @@
 /*  FUNCTION                                               RELEASE        */
 /*                                                                        */
 /*    _gx_vertical_list_scroll_info_get                   PORTABLE C      */
-/*                                                           6.1          */
+/*                                                           6.x          */
 /*  AUTHOR                                                                */
 /*                                                                        */
 /*    Kenneth Maxwell, Microsoft Corporation                              */
@@ -69,6 +69,10 @@
 /*  05-19-2020     Kenneth Maxwell          Initial Version 6.0           */
 /*  09-30-2020     Kenneth Maxwell          Modified comment(s),          */
 /*                                            resulting in version 6.1    */
+/*  xx-xx-xxxx     Ting Zhu                 Modified comments(s),         */
+/*                                            improved the calculation of */
+/*                                            the maximum scrolling value.*/
+/*                                            resulting in version 6.x    */
 /*                                                                        */
 /**************************************************************************/
 VOID _gx_vertical_list_scroll_info_get(GX_VERTICAL_LIST *list, ULONG style, GX_SCROLL_INFO *info)
