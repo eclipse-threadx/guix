522aafcc38ba1cb0d72c43a776f2e81eb568ce6f
Frédéric Desbiens <frederic.desbiens@eclipse-foundation.org>
2025-02-24 18:05:35 -0500

    Updated scripts for VS Community 2022.

--- scripts/build_guix_studio.cmd
+++ scripts/build_guix_studio.cmd
@@ -1,9 +1,11 @@
+@echo off
+
 rem Initialize the developer environment just like a developer box. Note that 'call' keyword that ensures that the script does not exist after 
 rem calling the other batch file.
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64 -winsdk=10.0.16299.0
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64 -winsdk=10.0.22621.0
 
-rem Set Python path
-SET PATH=%PATH%;"C:\Program Files\Python36";"C:\Program Files\Python36\scripts"
+rem Set Python path. Not needed if installed with WinGet.
+rem SET PATH=%PATH%;"C:\Program Files\Python36";"C:\Program Files\Python36\scripts"
 
 rem Save working directory so that we can restore it back after building everything. This will make developers happy and then 
 rem switch to the folder this script resides in. Don't assume absolute paths because on the build host and on the dev host the locations may be different.
